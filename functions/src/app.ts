618e9669a58e45d901286f4d7d9e9df24b976f05
Rômulo <romulo.tdsilva@gmail.com>
2018-05-16 23:55:03 -0300

    inicio api no firebase aeeee o/

---
+++ functions/src/app.ts
@@ -0,0 +1,46 @@
+import * as path from 'path';
+import * as express from 'express';
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
+
+export class App {
+    public express: express.Application;
+
+    constructor() {
+        this.express = express();
+
+        this.middleware();
+
+        this.routes();
+    }
+
+    middleware(): void {
+		this.express.use(logger('dev'));
+		this.express.use(bodyParser.json());
+		this.express.use(bodyParser.urlencoded({ extended: false }));
+		this.express.use(function (req, res, next) {
+			res.setHeader('Access-Control-Allow-Origin', '*');
+			res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+			res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+			next();
+		});
+    }
+    
+    routes(): void {
+		let router = express.Router();
+		// placeholder route handler
+		router.get('/', (req, res, next) => {
+			res.json({
+				message: 'Hello World!'
+			});
+		});
+		router.get('/hello', (req, res, next) => {
+			res.json({
+				message: 'Hello World!'
+			});
+		});
+		this.express.use('/', router);
+		// this.express.use('/api/autenticacao', new AuthRouter().router);
+		// this.express.use('/api/usuarios', new UsuarioRouter().router);
+	}
+}
